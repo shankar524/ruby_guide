0a9cf94d4f81c74f0a036de671f34d00911aeab1
Shankar Acharya <shankar.acharya524@gmail.com>
2020-02-08 13:11:07 +0545

    Add variable and ruby file

---
+++ strings.rb
@@ -0,0 +1,8 @@
+name = 'Apple, apple is fruit.'
+
+#Popular string method
+
+name.length
+name.upcase
+name.chars
+name.sub("Apple", "Banana")
---
+++ variables.rb
@@ -0,0 +1,27 @@
+name = 'Shankar'
+puts name
+
+height = 5.3
+puts height
+
+lucky_numeber = 7
+puts lucky_numeber
+
+symbols = :Shankar
+puts symbols
+
+#get input from console
+
+name = gets.chomp
+
+#integer divsion
+puts 22/7
+
+#float division
+puts 22.0/7
+
+#Constant
+PI = 3.67
+
+$global = 6.5
+@@class_varaible
