c6a231bd8b988f46c8518aebe55d834f8c21c7aa
Shankar Acharya <shankar.acharya524@gmail.com>
2020-02-17 00:01:46 +0545

    Add integers guide

---
+++ 03dataTypes/integers.rb
@@ -0,0 +1,22 @@
+# Arithmetic
+p 1 + 2
+p 2 * 3
+# Integer divide by Integer is Integer
+p 3 / 2
+p 10 - 11
+p 5 % 3
+p 5 ** 2
+
+
+# Some handy Integer methods
+p -2.abs        # Absolute value
+p 3.lcm 9       # LCM
+p 36.gcd 60     # HCF
+p 3.next        # Next integer
+p 4.odd?        # Is Odd?
+p 4.even?       # Is Even?
+p 4.positive?   # Is positive number?
+p 4.negative?   # Is negative number?
+p 0.zero?       # Is number zero?
+p 3.to_f        # Convert to float
+p 3.to_s        # Convert to string
